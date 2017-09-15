c986d0f564e88af38278d76d06b7d66752ba106e
volodymyr.kutsenko <volodymyr.kutsenko@oracle.com>
2017-09-15 09:08:42 +0300

    It looks like done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,59 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, ActivatedRoute, Routes } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: 'items-list',
+    pathMatch: 'full'
+  },
+  { path: 'items-list', 
+    component: ItemListComponent
+  },
+//    children: [ 
+      { 
+        path: 'items-list/item/:id/:status', 
+        component: ItemComponent 
+      },
+      { 
+        path: '**', 
+        component: PageNotFoundComponent 
+      }
+// ]
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    PageNotFoundComponent,
+    ItemListComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
